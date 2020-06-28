5288acc9089c70321181a4d5945af82efeb012d3
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2020-06-28 19:41:17 -0400

    Create server.js
    Solution built on the boilerplate.

---
+++ server.js
@@ -0,0 +1,33 @@
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(requestIp.mw())
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/whoami", function (req, res, next) {
+  var lang = req.headers["accept-language"] ;
+  const ip = req.clientIp;
+  var agent = useragent.lookup(req.headers['user-agent']);
+  res.json({ipaddress: ip, software: agent.device.toString(), language: lang});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
